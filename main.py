199c1d3476c55ba36de901568a8aef484423abde
Edson-Padilha <edsonp.bsi@gmail.com>
2024-12-19 11:29:04 -0300

    Inserindo cotação máxima e mínima do dia

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flet as ft 
 import requests
+import time
 
 def main(page: ft.Page):
     page.title = "Cotação do Dólar"
@@ -9,23 +10,36 @@ def main(page: ft.Page):
             response = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
             response.raise_for_status() # Verfica se a requisição foi bem sucedida
             data = response.json()
-            cotacao = data['USDBRL']['bid']
-            return cotacao
+            return data['USDBRL']
         
         except requests.exceptions.RequestException as e:
             print(f"Erro na requisição: {e}")
             return None
 
-    def update_quotation(e):
-        cotacao = get_dolar_quotation()
-        if cotacao:
-            texto_cotacao.value = f"Dólar: R$ {cotacao}"
+    
+    texto_cotacao = ft.Text("Cotação do Dólar: ", size=20)
+    texto_maxima = ft.Text(f"Máxima do dia: ", size=20) 
+    texto_minima = ft.Text(f"Mínima do dia: ", size=20)
+
+    def update_quotation():
+        data = get_dolar_quotation()
+
+        if data:
+            texto_cotacao.value = f"Dólar: R$ {data['bid']}"
+            texto_maxima.value = f"Máxima do dia: R$ {data['high']}"
+            texto_minima.value = f"Mínima do dia: R$ {data['low']}"
+        
         else:
             texto_cotacao.value = f"Erro ao obter cotação"
         page.update()
+    
+    def timer_tick(e):
+        update_quotation()
+        
+    update_quotation()
 
-    texto_cotacao = ft.Text("Cotação do Dólar: ", size=20)
-    botao_atualizar = ft.ElevatedButton("Atualizar", on_click=update_quotation)
+    # Configura o timer para chamar update_quotation a cada 1 segundo
+    page.on_show = lambda e: page.add(ft.Timer(1, on_tick=timer_tick))
 
-    page.add(texto_cotacao, botao_atualizar)
+    page.add(texto_cotacao, texto_maxima, texto_minima)
 ft.app(target=main)
